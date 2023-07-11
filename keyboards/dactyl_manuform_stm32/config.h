f9f968845e3d8888d855858f81b011d5c08c4672
Benjamin Krenn <benjamin.krenn@leftshift.one>
2023-07-11 11:46:18 +0200

    add stm32 dactyl

---
+++ keyboards/dactyl_manuform_stm32/config.h
@@ -0,0 +1,32 @@
+/*
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define HAL_USE_SERIAL TRUE
+//#define SPLIT_HAND_PIN B9
+//#define SERIAL_USART_DRIVER SD1
+//#define SERIAL_USART_TX_PAL_MODE 7
+//#define SERIAL_USART_SPEED 921600
+
+// full duplex uart
+#define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
+#define SERIAL_USART_TX_PIN B6     // USART TX pin
+#define SERIAL_USART_RX_PIN B7     // USART RX pin
+
+#define AUTO_SHIFT_TIMEOUT 250
